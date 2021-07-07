f17514aee7b46f6b3b4a20e8e42c6256981ca9d3
theofenique <theo@braineet.com>
2021-07-07 16:02:08 +0200

    feat(add): Connexion to ES is functionnal

--- api/config/db.go
+++ api/config/db.go
@@ -3,12 +3,25 @@ package config
 import (
 	"github.com/elastic/go-elasticsearch/v7"
 	"log"
+	"time"
 )
 
-func InitDB() elasticsearch.Client{
-	client, err := elasticsearch.NewDefaultClient()
-	if err != nil {
-		log.Fatalf("Cannot connect to ES: %s", err)
+func InitDB() *elasticsearch.Client{
+	for i := 0; i < 3; i++ {
+		log.Println("Trying to connect to ES...")
+		cfg := elasticsearch.Config{
+			Addresses: []string{
+				"http://elasticsearch:9200",
+			},
+		}
+		client, err := elasticsearch.NewClient(cfg)
+		if err != nil {
+			log.Println("Failed to connect, retrying in 15 seconds...")
+			time.Sleep(15 * time.Second)
+			continue
+		}
+		log.Println("Connected to the DB!")
+		return client
 	}
-	return *client
+	return nil
 }
